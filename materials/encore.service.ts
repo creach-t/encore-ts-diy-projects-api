c9a9578c10e84df8990eefc27792610c120991f9
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-06-10 19:44:54 +0200

    Add Materials service definition

---
+++ materials/encore.service.ts
@@ -0,0 +1,6 @@
+import { Service } from "encore.dev/service";
+
+// Define the Materials microservice
+// This demonstrates Encore's microservices capabilities
+// Each service can be developed, deployed, and scaled independently
+export default new Service("materials");
